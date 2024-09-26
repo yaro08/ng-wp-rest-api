a40eee32b688d9312367d164f00d71eca247e5a1
Yaroslav Trush <ytrush@umh.es>
2024-09-26 09:55:45 +0200

    fix: fixed interceptor

--- projects/ng-wp-rest-api/src/lib/utils/BaseUrlInterceptor.ts
+++ projects/ng-wp-rest-api/src/lib/utils/BaseUrlInterceptor.ts
@@ -1,18 +1,22 @@
-import { Inject, Injectable, Optional } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Inject, Injectable, Optional} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class BaseUrlInterceptor implements HttpInterceptor {
 
-    constructor(
-        @Inject('wpApiBaseUrl') private wpApiBaseUrl: string,
-        @Optional() @Inject('wpApiNamespace') private wpApiNamespace: string) {
-          this.wpApiNamespace = this.wpApiNamespace || 'wp/v2';
-    }
+  constructor(
+    @Inject('wpApiBaseUrl') private wpApiBaseUrl: string,
+    @Optional() @Inject('wpApiNamespace') private wpApiNamespace: string) {
+    this.wpApiNamespace = this.wpApiNamespace || 'wp/v2';
+  }
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const apiReq = request.clone({ url: `${this.wpApiBaseUrl}/${this.wpApiNamespace}/${request.url}` });
-        return next.handle(apiReq);
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (request.url.includes('wp-json/')) {
+      const apiReq = request.clone({url: `${this.wpApiBaseUrl}/${this.wpApiNamespace}/${request.url}`});
+      return next.handle(apiReq);
     }
+    return next.handle(request);
+  }
 }
+
