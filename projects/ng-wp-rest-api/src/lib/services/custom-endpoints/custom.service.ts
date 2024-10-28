c67ccc93cd948966aed3051edf55cfc480407228
Yaroslav Trush <ytrush@umh.es>
2024-10-28 13:34:00 +0100

    fix url

--- projects/ng-wp-rest-api/src/lib/services/custom-endpoints/custom.service.ts
+++ projects/ng-wp-rest-api/src/lib/services/custom-endpoints/custom.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Inject, Injectable, Optional} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
@@ -9,16 +9,21 @@ import {Custom} from './custom';
 })
 export class WpApiCustomService implements Custom {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    @Inject('wpApiBaseUrl') private wpApiBaseUrl: string,
+    @Optional() @Inject('wpApiNamespace') private wpApiNamespace: string) {
+    this.wpApiNamespace = this.wpApiNamespace || 'wp/v2';
+  }
 
   get<T>(url: string, id: number, options = {}): Observable<T> {
-    return this.http.get<T>(url.concat(id.toString()), options);
+    return this.http.get<T>(`${this.wpApiBaseUrl}/${this.wpApiNamespace}/${url.concat(id.toString())}`, options);
   }
 
   list<T>(url: string, options?: {}): Observable<T[]> {
     if (options) {
       options = { params: { ...options } };
     }
-    return this.http.get<T[]>(url, options);
+    return this.http.get<T[]>(`${this.wpApiBaseUrl}/${this.wpApiNamespace}/${url}`, options);
   }
 }
--- projects/ng-wp-rest-api/src/lib/utils/BaseUrlInterceptor.ts
+++ projects/ng-wp-rest-api/src/lib/utils/BaseUrlInterceptor.ts
@@ -12,10 +12,6 @@ export class BaseUrlInterceptor implements HttpInterceptor {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (request.url.includes('wp-json/')) {
-      const apiReq = request.clone({url: `${this.wpApiBaseUrl}/${this.wpApiNamespace}/${request.url}`});
-      return next.handle(apiReq);
-    }
     return next.handle(request);
   }
 }
